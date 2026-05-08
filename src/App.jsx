29b80cd9b1b37cf533657461363b7c4cd21c4dcf
Daniel Andeche <daniel.andeche@school.moringaschool.com>
2026-05-08 13:47:06 +0300

    added header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Header from './components/Header'
 import './App.css'
 
 function App() {
   return (
     <div className="app">
-      <h1>Personal Project Showcase App</h1>
+      <Header />
     </div>
   )
 }
